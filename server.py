101d863bde1d3a0779742bd806dbba888c75022d
Shane Aston <astonshane@gmail.com>
2016-03-16 22:37:46 -0400

    User page

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, abort
 import json
+import urllib2
 from pprint import pprint
 
 app = Flask(__name__)
@@ -22,12 +23,15 @@ def parseGroups():
     return users, projects
 
 
+def getUser(username):
+    data = json.loads(open('smallgroup.json').read())
+    for user in data.get("users", {}):
+        if user['github'] == username:
+            return user
+    return None
+
+
 @app.route("/")
-def hello():
-    return "hello world!"
-
-
-@app.route("/users/")
 def users():
     g.users, g.projects = parseGroups()
     return render_template("userlist.html")
@@ -35,7 +39,26 @@ def users():
 
 @app.route("/user/<username>")
 def user(username):
-    return render_template("user.html")
+    user_obj = getUser(username)
+    if user_obj is None:
+        abort(404)
+
+    data = json.loads(urllib2.urlopen('https://api.github.com/users/%s/events' % username).read())
+
+    user_obj['pic'] = "https://acrobatusers.com/assets/images/template/author_generic.jpg"
+    if len(data) > 0:
+        user_obj['pic'] = data[0]['actor']['avatar_url']
+
+    events = []
+
+    for event in data:
+        event_obj = {}
+        event_obj['type'] = event['type']
+        event_obj['repo'] = event['repo']['name']
+        event_obj['date'] = event['created_at']
+        events.append(event_obj)
+
+    return render_template("user.html", user=user_obj, events=events)
 
 
 @app.route("/project/<user>/<repo>")
