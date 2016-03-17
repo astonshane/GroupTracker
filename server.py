ae3f2ca88a2aa10b00e7f1dc9407d14f7120b113
Shane Aston <astonshane@gmail.com>
2016-03-16 21:54:08 -0400

    userlist page to list all of the users in the group

--- server.py
+++ server.py
@@ -1,11 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, g
+import json
+from pprint import pprint
 
 app = Flask(__name__)
 
 
+def parseGroups():
+    data = json.loads(open('smallgroup.json').read())
+
+    users = []
+    projects = set()
+
+    for user in data.get("users", {}):
+        pprint(user)
+        users.append(user)
+        projects.add(user['project'])
+
+    print users
+
+    print projects
+    return users, projects
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "hello world!"
+
+
+@app.route("/users/")
+def users():
+    g.users, g.projects = parseGroups()
+    return render_template("userlist.html")
+
+
+@app.route("/user/<username>")
+def user(username):
+    return render_template("user.html")
+
+
+@app.route("/project/<user>/<repo>")
+def project(user, repo):
+    return render_template("project.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
